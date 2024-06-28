221b3fa35d4dda6b1210c3cc703f59decd404182
jannatunNimeNishat <nishatnime100@gmail.com>
2024-06-29 01:05:27 +0600

    updated

---
+++ src/modules/hero/hero.model.ts
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+import { IHero } from "./hero.interface";
+
+const heroSchema = new Schema<IHero>(
+  {
+    greetings: { type: String, required: true },
+    name: { type: String, required: true },
+    dentation: { type: String, required: true },
+    tags: { type: [String], required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Hero = model<IHero>("Hero", heroSchema);
