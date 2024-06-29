3c19b2df6d9704fd39ba74bef7a28d8ba344164c
jannatunNimeNishat <nishatnime100@gmail.com>
2024-06-29 11:58:36 +0600

    Updated

--- src/modules/about/about.routes.ts
+++ src/modules/about/about.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { AboutController } from "./about.controller";
 
 const router = Router();
 
-
+router.post("/", AboutController.createAbout);
 
 export const AboutRoutes = router;
