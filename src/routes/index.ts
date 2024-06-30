5adbf0b4433984f063ce632a476dc6661338bc46
jannatunNimeNishat <nishatnime100@gmail.com>
2024-06-30 14:08:51 +0600

    Eduction routes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,29 +3,33 @@ import { HeroRoutes } from "../modules/hero/hero.routes";
 import { AboutRoutes } from "../modules/about/about.routes";
 import { SkillRoutes } from "../modules/skill/skill.routes";
 import { ProjectRoutes } from "../modules/project/project.routers";
+import { EducationRoutes } from "../modules/education/education.routes";
 
 const router = Router();
 
 const moduleRoutes = [
-    {
-        path:'/hero',
-        route:HeroRoutes
-    },
-    {
-        path:'/about',
-        route:AboutRoutes
-    },
-    {
-        path:'/skill',
-        route:SkillRoutes
-    },
-    {
-        path:'/project',
-        route:ProjectRoutes
-    },
-]
+  {
+    path: "/hero",
+    route: HeroRoutes,
+  },
+  {
+    path: "/about",
+    route: AboutRoutes,
+  },
+  {
+    path: "/skill",
+    route: SkillRoutes,
+  },
+  {
+    path: "/project",
+    route: ProjectRoutes,
+  },
+  {
+    path: "/education",
+    route: EducationRoutes,
+  },
+];
 
-moduleRoutes.forEach((route)=> router.use(route.path,route.route));
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
-
-export default router
+export default router;
