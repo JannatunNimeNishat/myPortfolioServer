9205380bae6fa10b66915bd3de42ce34a01b25aa
jannatunNimeNishat <nishatnime100@gmail.com>
2024-06-30 14:18:26 +0600

    education model  added

---
+++ src/modules/education/education.model.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+import { IEducation } from "./education.interface";
+
+const educationSchema = new Schema<IEducation>({
+  degreeName: { type: String, required: true },
+  institute: { type: String, required: true },
+  duration: { type: String, required: true },
+  result: { type: String, required: true },
+  skills: { type: [String], required: true },
+});
+
+export const Education = model<IEducation>("Education", educationSchema);
