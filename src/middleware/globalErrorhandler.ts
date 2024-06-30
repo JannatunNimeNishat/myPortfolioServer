948a8cc076dbeae2e982e4e4bf4c457907751ad5
jannatunNimeNishat <nishatnime100@gmail.com>
2024-06-30 20:12:02 +0600

    Updated

--- src/middleware/globalErrorhandler.ts
+++ src/middleware/globalErrorhandler.ts
@@ -1,15 +1,13 @@
 import { ErrorRequestHandler } from "express";
 
 const globalErrorhandler: ErrorRequestHandler = (error, req, res, next) => {
-    let statusCode = 500;
-    let message = error.message || "Something went wrong";
-    let stack
-  
-    return res.status(statusCode).json({
-      success:false,
-      message:message,
-      stack: stack,
-    })
-  };
-  
-  export default globalErrorhandler
+  let statusCode = 500;
+  let message = error.message || "Something went wrong";
+
+  return res.status(statusCode).json({
+    success: false,
+    message: message,
+  });
+};
+
+export default globalErrorhandler;
