cf738e3c62594aee2f9d00a52136557d272753a0
jannatunNimeNishat <nishatnime100@gmail.com>
2024-07-03 13:20:50 +0600

    Updated

--- src/modules/experience/experience.routes.ts
+++ src/modules/experience/experience.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import auth from "../../middleware/auth";
 import { USER_ROLE } from "../../constants";
+import { ExperienceController } from "./experience.controller";
 
 const router = Router();
-router.post('/',auth(USER_ROLE.admin))
-
+router.post("/", auth(USER_ROLE.admin), ExperienceController.createExperience);
 
 export const ExperienceRoutes = router;
